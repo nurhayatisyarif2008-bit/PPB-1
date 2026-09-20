90ce760cd96a25175fc8aba90291f4f335737b89
Nurhayati <nurhayatisyarif2008@gmail.com>
2026-09-20 13:34:09 +0700

    initial project

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+    int desimal = 10;
+    int octal = int.parse('13', radix: 8);
+    int binary = int.parse('1010', radix: 2);
+    int hexadecimal = 0xA;
+
+    print(desimal);
+    print(binary);
+    print(hexadecimal);
+    print(octal);
+    print('sistem bilangan Nurhayati');
+
+}
